9bafaaa3e781a261946ea8d02c3f81ae671d747a
Alec Luna <alecluna@csus.edu>
2018-10-23 17:53:55 -0700

    added sorting algo for arrival time

--- main.c
+++ main.c
@@ -4,12 +4,20 @@
 
 #define FILE_IN "input.1.txt"
 
+//compare function for out qsort() function
+int cmpfunc(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
 int main(void)
 {
 
-    int systemTime, arrivalTime, burstTime;
+    int process, arrivalTime, burstTime;
+    int systemTime = 0;
     int count = 0;
     char str[256];
+    int arrivalTimeList[15];
 
     FILE *input_file;
 
@@ -21,24 +29,38 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
+    //read input file first
     while (fgets(str, sizeof str, input_file))
     {
         char *p = strchr(str, '\n');
         if (p)
-            *p = 0; // remove the trailing newline, if
-
-        if (sscanf(str, "%d %d %d", &systemTime, &arrivalTime, &burstTime) == 3)
+            *p = 0; // weird hack to remove the trailing newline
+        if (sscanf(str, "%d %d %d", &process, &arrivalTime, &burstTime) == 3)
         {
-            printf("\nCurrent System Time:  %d\n", systemTime);
-            printf("\nArrival Time:   %d\n", arrivalTime);
-            printf("%d %d %d\n", systemTime, arrivalTime, burstTime);
-            /* do processing with i, j & k */
+            arrivalTimeList[count] = arrivalTime;
         }
-        else
-        {
-            perror("Error:");
-        }
+        count++;
     }
+
+    qsort(arrivalTimeList, count, sizeof(int), cmpfunc);
+    printf("\nAfter sorting the list is: \n");
+    for (int n = 0; n < 6; n++)
+    {
+        printf("%d ", arrivalTimeList[n]);
+    }
+    // while (burstTime >= 0)
+    // {
+    //     printf("\nCurrent burst time: %d", burstTime);
+    //     printf("\n<System Time     %d>", systemTime);
+    //     burstTime--;
+    //     //go to next process
+    //     if (burstTime == 0)
+    //     {
+    //         printf("\nCurrent burst time: %d", burstTime);
+    //         printf("\n<System Time     %d>", systemTime);
+    //         printf("\n****switch to next process");
+    //     }
+    // }
 
     fclose(input_file);
     return EXIT_SUCCESS;
